49e2d45e81e41ed6f02a9e1acb1631179bd83a17
Ronnie <16521174+beinnor@users.noreply.github.com>
2019-08-06 21:46:08 +0200

    fulfilled all required user stories

--- src/utils/defaultText.js
+++ src/utils/defaultText.js
@@ -1,46 +1,31 @@
-const defaultMarkdown = `# Welcome to my React Markdown Previewer!
+const defaultMarkdown = `# React Markdown Previewer!
+## by Beinnor
 
-## This is a sub-heading...
-### And here's some other cool stuff:
-  
-Heres some code, \`<div></div>\`, between 2 backticks.
+### Examples:
+
+Inline code: \`<div></div>\`, between 2 backticks.
 
 \`\`\`
-// this is multi-line code:
+// Multi-line code:
 
-function anotherExample(firstLine, lastLine) {
-  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-    return multiLineCode;
-  }
+function sum(a, b) {
+  return a + b;
 }
 \`\`\`
   
-You can also make text **bold**... whoa!
-Or _italic_.
-Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossing stuff out~~.
-
-There's also [links](https://www.freecodecamp.com), and
-> Block Quotes!
-
-And if you want to get really crazy, even tables:
-
-Wild Header | Crazy Header | Another Header?
------------- | ------------- | ------------- 
-Your content can | be here, and it | can be here....
-And here. | Okay. | I think we get it.
-
-- And of course there are lists.
-  - Some are bulleted.
-     - With different indentation levels.
-        - That look like this.
+Very **bold** text, or _italic_, or **_both!_**!
 
 
-1. And there are numbererd lists too.
-1. Use just 1s if you want! 
-1. But the list goes on...
-- Even if you use dashes or asterisks.
-* And last but not least, let's not forget embedded images:
+This is a project on [freeCodeCamp](https://www.freecodecamp.com)
+> freeCodeCamp.org is a tiny non-profit that's helping millions of people learn to code for free.
+
+
+- First list item
+- Second list item
+- Third list item
+- Fourth list item
+
+* This project was made using React
 
 ![React Logo w/ Text](https://goo.gl/Umyytc)
 `;
